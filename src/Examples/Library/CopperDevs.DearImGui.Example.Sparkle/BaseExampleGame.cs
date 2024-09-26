80a96e9dad5aa7c91b4f0756d44d1b4beb20d783
copperdevs <79734708+copperdevs@users.noreply.github.com>
2024-09-26 08:45:29 -0500

    file system organization

--- src/Examples/Library/CopperDevs.DearImGui.Example.Sparkle/BaseExampleGame.cs
+++ src/Examples/Library/CopperDevs.DearImGui.Example.Sparkle/BaseExampleGame.cs
@@ -1,4 +1,5 @@
 using CopperDevs.DearImGui.Renderer.Raylib;
+using CopperDevs.DearImGui.Renderer.Raylib.Raylib_CSharp;
 using Sparkle.CSharp;
 using SparkleLogger = Sparkle.CSharp.Logging.Logger;
 
@@ -6,35 +7,36 @@ namespace CopperDevs.DearImGui.Example.Sparkle;
 
 public class BaseExampleGame : Game
 {
-    public BaseExampleGame(GameSettings settings) : base(settings) {
+    public BaseExampleGame(GameSettings settings) : base(settings)
+    {
         SparkleLogger.Message += Utility.CustomLog;
     }
 
     protected override void Init()
     {
         base.Init();
-        
-        CopperImGui.Setup<RlImGuiRenderer>(true, true);
+
+        CopperImGui.Setup<RlImGuiRenderer<RlImGuiBinding>>(true, true);
         CopperImGui.ShowDearImGuiAboutWindow = true;
         CopperImGui.ShowDearImGuiDemoWindow = true;
         CopperImGui.ShowDearImGuiMetricsWindow = true;
         CopperImGui.ShowDearImGuiDebugLogWindow = true;
         CopperImGui.ShowDearImGuiIdStackToolWindow = true;
-        
+
         Utility.SetWindowStyling();
     }
 
     protected override void Draw()
     {
         base.Draw();
-        
+
         CopperImGui.Render();
     }
 
     protected override void OnClose()
     {
         base.OnClose();
-        
+
         CopperImGui.Shutdown();
     }
 }
